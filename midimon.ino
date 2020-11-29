d16344959e7119a20cda811661fe13f2ccddee76
Patrick Lang <patrick.lang@hotmail.com>
2020-11-28 16:53:38 -0800

    Filtering out MIDI start/stop/continue

--- midimon.ino
+++ midimon.ino
@@ -39,6 +39,12 @@ void onUsbSuspended(bool suspended)
 
 bool midimon_process(MidimonPort src, MidimonPort dst, u8 msg[3], u8 n)
 {
+	if (n > 0) {
+		// Good summary at https://wiki.cockos.com/wiki/index.php/MIDI_Specification
+		if (msg[0] == 0xFA || msg[0] == 0xFB || msg[0] == 0xFC) {
+			return false;
+		}
+	}
 	return true;
 }
 
